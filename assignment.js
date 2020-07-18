228ff7009622f215ea0fda32ce435bafa3c9997f
prosenjit31 <prosenjitghosh3182000@gmail.com>
2020-07-18 23:14:15 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,39 @@
+function feetToMile(feet){
+    var feet = mile /5280;
+    return feet;
+}
+  
+
+function woodCalculator(chair, fanitore){
+    var chairCount = chair *5;
+    var fanitoreCount = fanitore *5;
+    var totalWood = chairCount + fanitoreCount;
+    return totalWood;
+}
+
+
+function tinyFrind(sum){
+    var largest = sum[0];
+    for (var i = 0; i < sum.length; i++){
+        var currentNum = sum[i];
+        if(currentNum> length){
+            length = currentNum;
+        }
+    }
+    return length;
+
+}
+
+
+function brickCalculator(n){
+   var i = 1;
+   var brickCalculator = 1;
+   while (1<=n){
+       brickCalculator = brickCalculator *i;
+       i++;
+    
+   }
+   return brickCalculator;
+   
+}
+
